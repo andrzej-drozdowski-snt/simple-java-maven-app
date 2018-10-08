3853223328704412054c82e7c506b83c15a07105
A.T.D <andrzej.drozdowski@snt.pl>
2018-10-08 13:41:20 +0200

    test with error

--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -34,9 +34,9 @@ public class AppTest
     {
         App.main(null);
         try {
-            assertEquals("Hello From Application!" + System.getProperty("line.separator"), outContent.toString());
+            assertEquals("xHello From Application!" + System.getProperty("line.separator"), outContent.toString());
         } catch (AssertionError e) {
-            fail("\"message\" is not \"xHello From Application!\"");
+            fail("\"message\" is not \"Hello From Application!\"");
         }
     }
 
